8d4e6f5c3f0bb0683653c02858910c080934759f
hieucd <hieucd04@gmail.com>
2024-08-30 13:07:19 +0700

    refactor: Adjust JSX structure and stylings

--- app.tsx
+++ app.tsx
@@ -1,45 +1,33 @@
 import { registerRootComponent } from "expo";
 import { View } from "react-native";
 import { Color } from "@miniskylab/antimatter-color-scheme";
+import { DataList } from "@miniskylab/antimatter-data-list";
 import { DefaultIconSet } from "@miniskylab/antimatter-typography";
-import { TransactionTable } from "@miniskylab/antimatter-transaction-table";
 
 registerRootComponent(() => <App />);
 
 function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: Color.Ambient }}>
-      <TransactionTable
-        addNewTransactionButton={{
-          icon: DefaultIconSet.PlusCircle,
-          text: "Add New",
-          onPress: () => { alert("Lorem Ipsum"); }
-        }}
-        saveTransactionButton={{
-          icon: DefaultIconSet.FloppyDisk,
-          text: "Save",
-          onPress: () => { alert("Lorem Ipsum"); }
-        }}
-        deleteTransactionButton={{
-          icon: DefaultIconSet.TrashCan,
-          text: "Delete",
-          onPress: () => { alert("Lorem Ipsum"); }
-        }}
-        cancelButton={{
-          icon: DefaultIconSet.XMarkInsideCircle,
-          text: "Cancel",
-          onPress: () => { alert("Lorem Ipsum"); }
-        }}
-        customButton={{
-          icon: DefaultIconSet.Group,
-          text: "Lorem Ipsum: 99",
-          onPress: () => { alert("Lorem Ipsum"); }
-        }}
-        onSwitchMode={newMode => { alert("Lorem Ipsum"); }}
-        onChangeTransaction={newTransactionData => { alert("Lorem Ipsum"); }}
-        onSelectDate={newDate => { alert("Lorem Ipsum"); }}
-        onSelectTransaction={transactionId => { alert("Lorem Ipsum"); }}
-      />
+    <View style={{ flex: 1, alignItems: "center", backgroundColor: Color.Ambient }}>
+      <View style={{ flex: 1, width: "100%", maxWidth: 600 }}>
+        <DataList
+          button1={{
+            icon: DefaultIconSet.PlusCircle,
+            text: "Add New",
+            onPress: () => { alert("Lorem Ipsum"); }
+          }}
+          button2={{
+            icon: DefaultIconSet.Eye,
+            text: "Read-Only",
+            onPress: () => { alert("Lorem Ipsum"); }
+          }}
+          button3={{
+            icon: DefaultIconSet.XMarkInsideCircle,
+            text: "Cancel",
+            onPress: () => { alert("Lorem Ipsum"); }
+          }}
+        />
+      </View>
     </View>
   );
 }
